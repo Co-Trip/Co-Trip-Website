a4883d07e6723e942208018c07cd81effdd149c0
h1994st <h1994st@gmail.com>
2014-01-24 15:43:15 +0800

    automatically change background image

--- js/index-custom.js
+++ js/index-custom.js
@@ -1,18 +1,24 @@
-var canvas=document.getElementById('background-mask');
-var ctx=canvas.getContext('2d');
-ctx.fillStyle='#FF0000';
-ctx.fillRect(0,0,100,100);
-
 $(document).ready(function($) {
 	var num = Math.floor(Math.random() * 12 + 1);
 	var path = 'url(../Background/' + num + '.jpg)';
-	$('#background-image1').css({
-		opacity: 0,
-		backgroundImage: path
-	});
-	$('#background-image2').css({
-		opacity: 0,
-		backgroundImage: 'url(../Background/6.jpg)'
-	});
-	$('#background-image1').fadeTo('slow', 1);
+	$('#background-image1').css('background-image', path);
+	$('#background-image1').fadeIn(2000);
+	setInterval(function() {
+		changeBackgroundImage();
+	}, 8000);
 });
+
+changeBackgroundImage = function () {
+	var num = Math.floor(Math.random() * 12 + 1);
+	var path = 'url(../Background/' + num + '.jpg)';
+
+	if ($('#background-image1').is(":hidden")) {
+		$('#background-image1').css('background-image', path);
+		$('#background-image2').fadeOut(2000);
+		$('#background-image1').fadeIn(3000);
+	} else {
+		$('#background-image2').css('background-image', path);
+		$('#background-image1').fadeOut(2000);
+		$('#background-image2').fadeIn(3000);
+	};
+};
