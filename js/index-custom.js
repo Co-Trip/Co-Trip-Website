8c660fdb96b690991a53d4119f682177db2a423a
h1994st <h1994st@gmail.com>
2014-01-24 18:49:40 +0800

    new index page

--- js/index-custom.js
+++ js/index-custom.js
@@ -1,23 +1,23 @@
 $(document).ready(function($) {
 	var num = Math.floor(Math.random() * 12 + 1);
-	var path = 'url(../Background/' + num + '.jpg)';
-	$('#background-image1').css('background-image', path);
+	$('#background-image1').css('background-image', 'url(../Background/' + num + '.jpg)');
 	$('#background-image1').fadeIn(2000);
+	$('#background-mask').attr('src', '../Background/' + num + '.jpg');
 	setInterval(function() {
-		changeBackgroundImage();
+		num = Math.floor(Math.random() * 12 + 1);
+		changeBackgroundImage(num);
 	}, 8000);
 });
 
-changeBackgroundImage = function () {
-	var num = Math.floor(Math.random() * 12 + 1);
-	var path = 'url(../Background/' + num + '.jpg)';
-
+changeBackgroundImage = function (num) {
 	if ($('#background-image1').is(":hidden")) {
-		$('#background-image1').css('background-image', path);
+		$('#background-image1').css('background-image', 'url(../Background/' + num + '.jpg)');
+		$('#background-mask').attr('src', '../Background/' + num + '.jpg');
 		$('#background-image2').fadeOut(2000);
 		$('#background-image1').fadeIn(3000);
 	} else {
-		$('#background-image2').css('background-image', path);
+		$('#background-image2').css('background-image', 'url(../Background/' + num + '.jpg)');
+		$('#background-mask').attr('src', '../Background/' + num + '.jpg');
 		$('#background-image1').fadeOut(2000);
 		$('#background-image2').fadeIn(3000);
 	};
